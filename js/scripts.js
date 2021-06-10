22e4d63e9cfcf5d18918c2f8c0a834bf4469357f
lukellyk <luke-k@live.com>
2021-06-10 12:45:43 +1000

    added user journey map

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('journey-map').onclick = () => {
+
+    const url = 'https://github.com/lukellyk/usyd-biodesign-2021/raw/d8340392dd899e8914bd3a74f85e38ccd923ced5/User%20Journey%20Map2.png'
+
+	window.open(url, '_blank').focus();
+    
+}
